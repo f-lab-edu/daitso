36bb01a7915d2feddbc7a8d4604ed94fcb69bbe1
Seung <applecv3@naver.com>
2021-06-23 05:19:01 -0700

    기능 추가

---
+++ src/main/java/com/flab/daitso/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.flab.daitso.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "daitso/product/category")
+public class OrderController {
+
+    
+}
