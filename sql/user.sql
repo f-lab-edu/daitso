df7f5e04dc69a4c85f4d6858e5132c52ae90a116
oopsys6 <operasystem6@naver.com>
2021-06-24 23:25:48 +0900

    docs: user table 추가

---
+++ sql/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `user` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT=1,
+    `userId` varchar(45) UNIQUE NOT NULL,
+    `userPassword` varchar(100) NOT NULL,
+    `name` varchar(15) NOT NULL,
+    `phoneNumber` varchar(15) NOT NULL,
+    `registrationDate`  varchar (15) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
