73abbfc9e6aa4c78dafe5d2a35397a0aefbd42b4
oopsys6 <operasystem6@naver.com>
2021-06-25 19:03:20 +0900

    docs: role 컬럼 추가

--- sql/user.sql
+++ sql/user.sql
@@ -4,6 +4,7 @@ CREATE TABLE `user` (
     `userPassword` varchar(100) NOT NULL,
     `name` varchar(15) NOT NULL,
     `phoneNumber` varchar(15) NOT NULL,
+    `role` varchar(15) NOT NULL,
     `registrationDate`  varchar (15) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
