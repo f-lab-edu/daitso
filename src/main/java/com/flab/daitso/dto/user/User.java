28aa5b85070358a3339432576709935178b4b204
oopsys6 <operasystem6@naver.com>
2021-06-25 19:04:23 +0900

    feat: role 추가

--- src/main/java/com/flab/daitso/dto/user/User.java
+++ src/main/java/com/flab/daitso/dto/user/User.java
@@ -1,6 +1,5 @@
 package com.flab.daitso.dto.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,15 +11,17 @@ public class User {
     private String userPassword;
     private String name;
     private String phoneNumber;
+    private Role role;
     private String registrationDate;
 
     @Builder
-    public User(Long id, String userId, String userPassword, String name, String phoneNumber, String registrationDate) {
+    public User(Long id, String userId, String userPassword, String name, String phoneNumber, Role role, String registrationDate) {
         this.id = id;
         this.userId = userId;
         this.userPassword = userPassword;
         this.name = name;
         this.phoneNumber = phoneNumber;
+        this.role = role;
         this.registrationDate = registrationDate;
     }
 }
