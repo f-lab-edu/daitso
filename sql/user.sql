fb03ca309ac96bbd95ae758750a2c25956762f7a
oopsys6 <operasystem6@naver.com>
2021-06-25 21:31:05 +0900

    refactor: collate 제거

--- sql/user.sql
+++ sql/user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `user` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT=1,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `userId` varchar(45) UNIQUE NOT NULL,
     `userPassword` varchar(100) NOT NULL,
     `name` varchar(15) NOT NULL,
@@ -7,4 +7,4 @@ CREATE TABLE `user` (
     `role` varchar(15) NOT NULL,
     `registrationDate`  varchar (15) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
