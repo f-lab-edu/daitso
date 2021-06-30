cb71cc40ff8059ef891dcb4640640e766ad4192d
oopsys6 <operasystem6@naver.com>
2021-06-30 22:32:34 +0900

    test: product service test 코드 작성

--- src/test/java/com/flab/daitso/service/ProductServiceTest.java
+++ src/test/java/com/flab/daitso/service/ProductServiceTest.java
@@ -1,11 +1,8 @@
 package com.flab.daitso.service;
 
-
 import com.flab.daitso.dto.product.ProductDto;
 import com.flab.daitso.error.exception.product.NotFoundException;
-import com.flab.daitso.error.exception.product.SoldOutException;
 import com.flab.daitso.mapper.ProductMapper;
-import com.flab.daitso.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,9 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -25,37 +23,85 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductServiceTest {
 
     @Autowired
-    ProductService productService;
+    ProductMapper productMapper;
 
     @Autowired
-    ProductMapper productMapper;
+    ProductService productService;
+
+    @Test
+    @DisplayName("상품_등록_테스트")
+    public void 상품_등록_테스트() {
+        ProductDto productDto = new ProductDto.Builder()
+                .categoryId(1)
+                .name("test")
+                .price(10000)
+                .content("test 상품입니다.")
+                .build();
+
+        productService.registerProduct(productDto);
+        ProductDto product = productService.findByName("test");
+        assertThat(product.getName()).isEqualTo(productDto.getName());
+    }
 
     @Test
     @DisplayName("해당 카테고리에 속하는 모든 상품 목록을 제대로 검색하는지 테스트")
-    public void getProducts() throws Exception {
-        // ?? 어떻게 태스트함 뭔 기준??
+    public void 카테고리별_상품_목록_테스트() {
+        ProductDto productDto1 = new ProductDto.Builder()
+                .categoryId(1)
+                .name("test")
+                .price(10000)
+                .content("test 상품입니다.")
+                .build();
+
+        ProductDto productDto2 = new ProductDto.Builder()
+                .categoryId(1)
+                .name("test1")
+                .price(20000)
+                .content("test1 상품입니다.")
+                .build();
+
+        productService.registerProduct(productDto1);
+        productService.registerProduct(productDto2);
+
+        List<ProductDto> productAll = productService.findProductAll(1);
+        assertThat(productAll.size()).isEqualTo(2);
+        assertThat(productAll.get(0).getName()).isEqualTo("test");
     }
 
     @Test
     @DisplayName("상품 아이디로 해당 상품을 제대로 검색하는지 테스트")
-    public void getProduct() throws Exception {
+    public void 상품_아이디로_검색_테스트() {
+        ProductDto productDto = new ProductDto.Builder()
+                .categoryId(1)
+                .name("test1")
+                .price(20000)
+                .content("test1 상품입니다.")
+                .build();
+        productService.registerProduct(productDto);
 
         // 상품 아이디를 이용해 상품을 제대로 검색하는지 테스트
-        ProductDto product = productService.getProduct(4);
-        assertThat(product.getPid()).isEqualTo(4);
-
-        // 없는 상품 아이디로 검색했을때 NotFoundException 을 제대로 반환 하는지 테스트
-        assertThrows(NotFoundException.class, () -> productService.getProduct(-2));
-
-        // 품절된 상품을 검색했을때 SoldOutException 을 제대로 반환 하는지 테스트
-        assertThrows(SoldOutException.class, () -> productService.getProduct(5));
-
+        ProductDto product = productService.findProductById(1L);
+        assertThat(product.getPid()).isEqualTo(1L);
     }
 
     @Test
-    @DisplayName("하나의 상품을 제대로 등록하는지 테스트")
-    public void registerProduct() throws Exception{
+    @DisplayName("상품 아이디로 해당 상품을 제대로 검색하는지 테스트")
+    public void 상품_이름으로_검색_테스트() {
+        ProductDto productDto = new ProductDto.Builder()
+                .categoryId(1)
+                .name("test1")
+                .price(20000)
+                .content("test1 상품입니다.")
+                .build();
+        productService.registerProduct(productDto);
 
+        ProductDto product = productService.findByName("test1");
+        assertThat(product.getName()).isEqualTo(productDto.getName());
     }
 
+    @Test
+    @DisplayName("없는 상품 아이디로 검색했을 때 테스트")
+    public void 존재하지_않는_상품_테스트() {
+        assertThrows(NotFoundException.class, () -> productService.findProductById(-2L));
+    }
 }
