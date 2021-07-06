8d75b9685e2f1aa7e260ddb6031d84d7c18d222a
oopsys6 <operasystem6@naver.com>
2021-07-07 00:23:29 +0900

    test: product service, controller 삭제 기능 test code 작성

--- src/test/java/com/flab/daitso/controller/ProductControllerTest.java
+++ src/test/java/com/flab/daitso/controller/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.flab.daitso.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flab.daitso.dto.product.Category;
 import com.flab.daitso.dto.product.ProductDto;
 import com.flab.daitso.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,8 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,40 +55,36 @@ public class ProductControllerTest {
     @DisplayName("상품 등록 테스트")
     public void 상품_등록_테스트() throws Exception {
         ProductDto productDto = new ProductDto.Builder()
-                .categoryId(1)
-                .name("test2")
-                .price(10000)
+                .name("test1")
+                .price(10000L)
                 .content("test 상품입니다.")
                 .build();
 
         mvc.perform(post("/manage/register")
                 .content(objectMapper.writeValueAsString(productDto))
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("카테고리별 상품 목록 조회 테스트")
     public void 카테고리별_상품_목록_조회_테스트() throws Exception {
         ProductDto productDto1 = new ProductDto.Builder()
-                .categoryId(1)
                 .name("test1")
-                .price(10000)
+                .price(10000L)
                 .content("test 상품입니다.")
                 .build();
 
         ProductDto productDto2 = new ProductDto.Builder()
-                .categoryId(1)
                 .name("test2")
-                .price(20000)
+                .price(20000L)
                 .content("test2 상품입니다.")
                 .build();
 
         ProductDto productDto3 = new ProductDto.Builder()
-                .categoryId(1)
                 .name("test3")
-                .price(30000)
+                .price(30000L)
                 .content("test3 상품입니다.")
                 .build();
 
@@ -96,39 +92,55 @@ public class ProductControllerTest {
         productService.registerProduct(productDto2);
         productService.registerProduct(productDto3);
 
-        mvc.perform(get("/np/categories/1")
+        mvc.perform(get("/np/categories/")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
 
+
     @Test
     @DisplayName("상품 아이디 해당 상품을 제대로 검색하는지 테스트")
     public void 상품_아이디로_검색_테스트() throws Exception {
         ProductDto productDto = new ProductDto.Builder()
-                .categoryId(1)
                 .name("test1")
-                .price(20000)
+                .price(20000L)
                 .content("test1 상품입니다.")
                 .build();
         productService.registerProduct(productDto);
 
         ProductDto product = productService.findProductByName("test1");
 
-        mvc.perform(get("/vp/products/" + product.getPid())
+        mvc.perform(get("/vp/products/" + product.getProductId())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
 
+
     @Test
     @DisplayName("상품 아이디 해당 상품을 제대로 검색하는지 테스트")
     public void 존재하지_않는_상품_테스트() throws Exception {
-
         mvc.perform(get("/vp/products/" + -1)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string("NotFoundException"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("상품 아이디로 상품 삭제")
+    public void 상품_아이디로_상품_삭제() throws Exception {
+        ProductDto product = new ProductDto.Builder()
+                .name("test1")
+                .price(20000L)
+                .content("test1 상품입니다.")
+                .build();
+        productService.registerProduct(product);
+
+        mvc.perform(delete("/manage/delete/" + product.getProductId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andDo(print());
     }
 }
--- src/test/java/com/flab/daitso/service/ProductServiceTest.java
+++ src/test/java/com/flab/daitso/service/ProductServiceTest.java
@@ -14,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,7 +39,7 @@ class ProductServiceTest {
     public void 상품_등록_테스트() {
         ProductDto productDto = new ProductDto.Builder()
                 .name("test")
-                .price(10000)
+                .price(10000L)
                 .content("test 상품입니다.")
                 .build();
 
@@ -50,14 +53,14 @@ class ProductServiceTest {
     public void 존재하는_상품_테스트() {
         ProductDto productDto = new ProductDto.Builder()
                 .name("test")
-                .price(10000)
+                .price(10000L)
                 .content("test 상품입니다.")
                 .build();
         productService.registerProduct(productDto);
 
         ProductDto productDto1 = new ProductDto.Builder()
                 .name("test")
-                .price(10000)
+                .price(10000L)
                 .content("test 상품입니다.")
                 .build();
         assertThrows(DuplicateProductNameException.class, () -> productService.registerProduct(productDto1));
@@ -66,13 +69,16 @@ class ProductServiceTest {
     @Test
     @DisplayName("상품에 카테고리 지정")
     public void 상품에_카테고리_지정_테스트() {
+        List<ProductDto> products = new ArrayList<>();
+
         ProductDto productDto1 = new ProductDto.Builder()
                 .name("test")
-                .price(10000)
+                .price(10000L)
                 .content("test 상품입니다.")
                 .build();
 
         productService.registerProduct(productDto1);
+        products.add(productDto1);
 
         Category sportsCategory = new Category("sports");
         categoryService.saveCategory(sportsCategory);
@@ -81,39 +87,36 @@ class ProductServiceTest {
         sportsCategory.addChildCategory(soccerCategory);
 
         Long categoryId = categoryService.saveCategory(soccerCategory);
-        Category category = productService.saveProductInCategory(categoryId, productDto1.getProductId());
 
-        System.out.println("##" + category.getProducts().get(0).getProductId());
+        Category category = productService.saveProductInCategory(categoryId, products);
 
         assertThat(category.getProducts().get(0).getProductId()).isEqualTo(productDto1.getProductId());
     }
 
 
+    @Test
+    @DisplayName("상품 아이디로 해당 상품을 제대로 검색하는지 테스트")
+    public void 상품_아이디로_검색_테스트() {
+        ProductDto productDto = new ProductDto.Builder()
+                .name("test1")
+                .price(20000L)
+                .content("test1 상품입니다.")
+                .build();
+        productService.registerProduct(productDto);
 
-//    @Test
-//    @DisplayName("상품 아이디로 해당 상품을 제대로 검색하는지 테스트")
-//    public void 상품_아이디로_검색_테스트() {
-//        ProductDto productDto = new ProductDto.Builder()
-//                .categoryId(1)
-//                .name("test1")
-//                .price(20000)
-//                .content("test1 상품입니다.")
-//                .build();
-//        productService.registerProduct(productDto);
-//
-//        ProductDto product = productService.findProductByName("test1");
-//
-//        // 상품 아이디를 이용해 상품을 제대로 검색하는지 테스트
-//        ProductDto findProduct = productService.findProductById(product.getPid());
-//        assertThat(product.getPid()).isEqualTo(findProduct.getPid());
-//    }
+        ProductDto product = productService.findProductByName("test1");
+
+        // 상품 아이디를 이용해 상품을 제대로 검색하는지 테스트
+        ProductDto findProduct = productService.findProductById(product.getProductId());
+        assertThat(product.getProductId()).isEqualTo(findProduct.getProductId());
+    }
 
     @Test
     @DisplayName("상품 이름로 해당 상품을 제대로 검색하는지 테스트")
     public void 상품_이름으로_검색_테스트() {
         ProductDto productDto = new ProductDto.Builder()
                 .name("test1")
-                .price(20000)
+                .price(20000L)
                 .content("test1 상품입니다.")
                 .build();
         productService.registerProduct(productDto);
@@ -126,5 +129,51 @@ class ProductServiceTest {
     @DisplayName("없는 상품 아이디로 검색했을 때 테스트")
     public void 존재하지_않는_상품_테스트() {
         assertThrows(NotFoundException.class, () -> productService.findProductById(-2L));
+    }
+
+    @Test
+    @DisplayName("상품 아이디로 상품 삭제")
+    public void 상품_아이디로_상품_삭제() {
+        ProductDto productDto = new ProductDto.Builder()
+                .name("test1")
+                .price(20000L)
+                .content("test1 상품입니다.")
+                .build();
+        productService.registerProduct(productDto);
+
+        Long productId = productDto.getProductId();
+        productService.deleteProduct(productId);
+
+        assertThrows(NotFoundException.class, () -> productService.findProductById(productId));
+    }
+
+    @Test
+    @DisplayName("카테고리 내에 존재하는 상품 삭제")
+    public void 카테고리_내_존재하는_상품_삭제() {
+        List<ProductDto> products = new ArrayList<>();
+
+        ProductDto productDto1 = new ProductDto.Builder()
+                .name("test")
+                .price(10000L)
+                .content("test 상품입니다.")
+                .build();
+
+        productService.registerProduct(productDto1);
+        products.add(productDto1);
+
+        Category sportsCategory = new Category("sports");
+        categoryService.saveCategory(sportsCategory);
+
+        Category soccerCategory = new Category("soccer");
+        sportsCategory.addChildCategory(soccerCategory);
+
+        Long categoryId = categoryService.saveCategory(soccerCategory);
+
+        productService.saveProductInCategory(categoryId, products);
+
+        Long productId = productDto1.getProductId();
+        productService.deleteProduct(productId);
+
+        assertThrows(NotFoundException.class, () -> productService.findProductById(productId));
     }
 }
