b303147b24ac9d76a5eafbd4c4ba3b10e7bf312a
oopsys6 <operasystem6@naver.com>
2021-07-15 23:40:56 +0900

    refactor: 배탈 타입 추가 및 int -> bigint로 변경

--- sql/product.sql
+++ sql/product.sql
@@ -5,11 +5,11 @@ create table product(
    content text NOT NULL,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   quantity int default 1,
-   score int default 0,
+   quantity BIGINT default 1,
+   score BIGINT default 0,
    main_image VARCHAR(255),
    detail_image VARCHAR(255),
-   delivery_fee BIGINT,
+   delivery_charge_type VARCHAR(100),
    deleted char(1) default 'N',
    PRIMARY KEY (product_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
