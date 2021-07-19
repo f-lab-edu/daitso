172697c58faaba8524d05937e378a96935218b92
oopsys6 <operasystem6@naver.com>
2021-07-20 01:11:48 +0900

    refactor: transactional 주석 해제

--- src/test/java/com/flab/daitso/controller/ProductControllerTest.java
+++ src/test/java/com/flab/daitso/controller/ProductControllerTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-//@Transactional
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public class ProductControllerTest {
