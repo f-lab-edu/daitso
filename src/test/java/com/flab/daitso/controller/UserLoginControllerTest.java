b69f443d7866d5b0ec80a40e73814176c775be8a
oopsys6 <operasystem6@naver.com>
2021-07-23 18:55:13 +0900

    test: User 관련 서비스 테스트

--- src/test/java/com/flab/daitso/controller/UserLoginControllerTest.java
+++ src/test/java/com/flab/daitso/controller/UserLoginControllerTest.java
@@ -1,10 +1,9 @@
 package com.flab.daitso.controller;
 
-/*import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flab.daitso.dto.user.UserLoginRequest;
 import com.flab.daitso.dto.user.UserRegister;
 import com.flab.daitso.service.UserService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,6 +30,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class UserLoginControllerTest {
 
+    private static String userEmail = "test1@naver.com";
+    private static String userPassword = "1q2w3e4r!";
+    private static String confirmUserPassword = "1q2w3e4r!";
+    private static String name = "park";
+    private static String phoneNumber = "010-1111-2222";
+
+    private static UserRegister userRegister = new UserRegister(userEmail, userPassword, confirmUserPassword, name, phoneNumber);
+
     @Autowired
     private MockMvc mvc;
 
@@ -49,18 +56,15 @@ class UserLoginControllerTest {
                 .webAppContextSetup(context)
                 .build();
     }
+
     @Test
     @DisplayName("로그인 테스트")
     public void 로그인_테스트() throws Exception {
-        String userEmail = "test13@naver.com";
-        String userPassword = "1q2w3e4r!";
-
-        UserRegister userRegister = new UserRegister(userEmail, userPassword, "test", "010-1111-2222","seoul");
         userService.signup(userRegister);
 
         UserLoginRequest userLoginRequest = new UserLoginRequest(userEmail, userPassword);
 
-        mvc.perform(post("/users/login")
+        mvc.perform(post("/api/users/login")
                 .content(objectMapper.writeValueAsString(userLoginRequest))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -68,14 +72,14 @@ class UserLoginControllerTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 아이디 로그인 실패 테스트")
+    @DisplayName("존재하지 않는 아이디, 로그인 실패 테스트")
     public void 로그인_실패_테스트() throws Exception {
         String userEmail = "test000@naver.com";
         String userPassword = "1q2w3e4r!";
 
         UserLoginRequest userLoginRequest = new UserLoginRequest(userEmail, userPassword);
 
-        mvc.perform(post("/users/login")
+        mvc.perform(post("/api/users/login")
                 .content(objectMapper.writeValueAsString(userLoginRequest))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
@@ -84,16 +88,43 @@ class UserLoginControllerTest {
     }
 
     @Test
-    @DisplayName("로그아웃 성공")
+    @DisplayName("아이디는 존재하지만 비밀번호 오류, 로그인 실패 테스트")
+    public void 비밀번호_오류_로그인_실패_테스트() throws Exception {
+        mvc.perform(post("/api/users/signup")
+                .content(objectMapper.writeValueAsString(userRegister))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        String userPassword = "1q2w3e4r!!";
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest(userEmail, userPassword);
+
+        mvc.perform(post("/api/users/login")
+                .content(objectMapper.writeValueAsString(userLoginRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("WrongPasswordException"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("세션이 존재할 때, 로그아웃 성공")
     public void 로그아웃_테스트() throws Exception {
 
         MockHttpSession session = new MockHttpSession();
 
         session.setAttribute("USER_ID", 1);
 
-        mvc.perform(post("/users/logout")
+        mvc.perform(post("/api/users/logout")
                 .session(session)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
-}*/
+
+    @Test
+    @DisplayName("세션이 존재하지 않을 때, 로그아웃 실패")
+    public void 로그아웃_실패_테스트() throws Exception {
+        mvc.perform(post("/api/users/logout")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+}
