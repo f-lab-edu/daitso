3c5899802f35ec1e1a7a4fcfcca216024c149ea3
oopsys6 <operasystem6@naver.com>
2021-07-24 10:33:38 +0900

    feat: 외래키 cascade 추가

--- sql/product_category.sql
+++ sql/product_category.sql
@@ -3,6 +3,6 @@ CREATE TABLE product_category (
     category_id BIGINT,
     product_id BIGINT,
     PRIMARY KEY (product_category_id),
-    FOREIGN KEY (category_id) REFERENCES category (category_id),
-    FOREIGN KEY (product_id) REFERENCES product (product_id)
+    FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
